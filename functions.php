37d9ae1838c4a5e69411d46329af3800a41bddfa
sabest2022 <askarians@me.com>
2023-04-23 20:36:01 +0200

    changing with new template

--- functions.php
+++ functions.php
@@ -1,12 +1,17 @@
 <?php
 function min_funktion()
 {
-    echo '<p>Detta är mitt eget innehåll som visas innan huvudinnehållet på WooCommerce-sidor</p>';
+    echo '<h3> >>> Detta är mitt eget innehåll som visas innan huvudinnehållet på WooCommerce-sidor <<< </h3>';
 }
 add_action('woocommerce_before_main_content', 'min_funktion');
 
-function remove_funktion()
+function remove_short_description()
 {
-    remove_action('footer', 'standard_widget');
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
 }
-add_action('init', 'remove_funktion');
+add_action('init', 'remove_short_description');
+function replace_short_description()
+{
+    echo '<h3> >> Description removed by remove_action and replaced with this text! << <h3>';
+}
+add_action('woocommerce_single_product_summary', 'replace_short_description');
